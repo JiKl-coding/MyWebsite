c5ec6dbed9d74c375e0ea5d8183f18770cf5a489
JiKl Coding <klatovskyjiri@gmail.com>
2023-09-04 13:38:05 +0200

    javascript add

--- script.js
+++ script.js
@@ -5,3 +5,58 @@ toggleButton.addEventListener('click', (e) => {
   e.preventDefault();
   navbarLinks.classList.toggle('active');
 });
+
+function getCurrentPageName() {
+  const url = window.location.href;
+  
+  const urlParts = url.split("/");
+  
+  let currentPage = urlParts[urlParts.length - 1];
+  
+  if (currentPage.includes("?")) {
+    currentPage = currentPage.split("?")[0];
+  }
+  
+  if (currentPage.includes(".")) {
+    currentPage = currentPage.split(".")[0];
+  }
+  
+  return currentPage;
+}
+
+const currentPageName = getCurrentPageName();
+
+const stackImg1 = document.getElementsByClassName("square-image")[0];
+const stackImg2 = document.getElementsByClassName("square-image")[1];
+
+let imageLinks = []
+
+if (currentPageName === "index%20eng" || currentPageName === "index") {
+  imageLinks = [
+    "./assets/img/logos/python.png",
+    "./assets/img/logos/js.png",
+    "./assets/img/logos/html5.png",
+    "./assets/img/logos/css.png",
+  ];
+}
+else {
+  imageLinks = [
+    "../../assets/img/logos/python.png",
+    "../../assets/img/logos/js.png",
+    "../../assets/img/logos/html5.png",
+    "../../assets/img/logos/css.png",
+  ];
+}
+
+let currentIndex = 0;
+
+function changeImage() {
+  stackImg1.src = imageLinks[currentIndex];
+  currentIndex = (currentIndex + 1) % imageLinks.length;
+  setTimeout(function () {
+    stackImg2.src = imageLinks[currentIndex];
+    currentIndex = (currentIndex + 1) % imageLinks.length;
+  }, 1000);
+}
+
+const intervalId = setInterval(changeImage, 2000);
